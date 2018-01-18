1103c8e35ea9a16506020719436cc7053ff8418c
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-01-19 00:56:01 +0100

    Use env variables

--- pobierz-pogode.py
+++ pobierz-pogode.py
@@ -1,52 +1,54 @@
+from os.path import join, dirname
+from os import environ
 import psycopg2
 import datetime
 from time import sleep
 from requests import get
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 SLEEP_TIME = 1
 CITY_ID = '7531836' # Poznan
-OPENWEATHER_APPID = '***REMOVED***'
-#FILE_NAME = './t1'
-FILE_NAME = '/home/pi/stacja/t1'
+FILE_NAME = environ.get('SENSOR_OUTPUT_FILENAME')
+OPENWEATHER_APPID = environ.get('OPENWEATHER_APPID')
+PGUSER = environ.get('PGUSER')
+PGDATABASE = environ.get('PGDATABASE')
+PGPASSWORD = environ.get('PGPASSWORD')
 
 
-def getTempFromFile(file):
+def getTempFromFile():
+  file = open(FILE_NAME, 'r')
   lines = file.readlines()
   temp = int(lines[1].split()[-1][2:]) / 1000
+  file.close()
   return temp
 
+def getTempFromApi():
+  payload = { 'id': CITY_ID, 'appid': OPENWEATHER_APPID, 'units': 'metric' }
+  req = get('http://api.openweathermap.org/data/2.5/weather', params=payload)
+  output = req.json()
+  temp = output['main']['temp']
+  return temp
 
-conn = psycopg2.connect(dbname=DBNAME, user=DBUSER, password=DBPASS)
-cursor = conn.cursor()
-
-def loop():
-  while True:
-    payload = { 'id': CITY_ID, 'appid': OPENWEATHER_APPID, 'units': 'metric' }
-    req = get('http://api.openweathermap.org/data/2.5/weather', params=payload)
-    #print(req.url)
-    output = req.json()
-    apiTemp = output['main']['temp']
-
-    f = open(FILE_NAME, "r")
-    sensorTemp = getTempFromFile(f)
-    f.close()
-
-    # TODO: save api temp to db
-    cursor.execute("INSERT INTO readings (sensor,api) VALUES (%s,%s)", [sensorTemp, apiTemp])
-    conn.commit()
-
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(now, " saved sensor temp ", str(sensorTemp), " and api temp ", str(apiTemp))
-
-    sleep(SLEEP_TIME)
-
-
-try:
-  loop()
-except KeyboardInterrupt:
+def saveTempsToDb(sensorTemp, apiTemp):
+  conn = psycopg2.connect(dbname=PGDATABASE, user=PGUSER, password=PGPASSWORD)
+  cursor = conn.cursor()
+  cursor.execute('INSERT INTO readings (sensor,api) VALUES (%s,%s)', [sensorTemp, apiTemp])
+  conn.commit()
   cursor.close()
   conn.close()
-finally:
-  cursor.close()
-  conn.close()
+
+def log(sensorTemp, apiTemp):
+  now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  print(now, ' saved sensor temp ', str(sensorTemp), ' and api temp ', str(apiTemp))
+
+
+while True:
+  apiTemp = getTempFromApi()
+  sensorTemp = getTempFromFile()
+
+  saveTempsToDb(sensorTemp, apiTemp)
+  log(sensorTemp, apiTemp)
+
+  sleep(SLEEP_TIME)
