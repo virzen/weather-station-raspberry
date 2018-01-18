99ebc4685dd1c36bf14211702750080c24e6a764
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-01-19 00:14:08 +0100

    Get weather data from api and save it

--- pobierz-pogode.py
+++ pobierz-pogode.py
@@ -6,7 +6,9 @@ from requests import get
 
 SLEEP_TIME = 1
 CITY_ID = '7531836' # Poznan
-OPENWEATHER_APPID = '***REMOVED***';
+OPENWEATHER_APPID = '***REMOVED***'
+#FILE_NAME = './t1'
+FILE_NAME = '/home/pi/stacja/t1'
 
 
 def getTempFromFile(file):
@@ -15,28 +17,26 @@ def getTempFromFile(file):
   return temp
 
 
-conn = psycopg2.connect("dbname=virzen user=virzen")
+conn = psycopg2.connect(dbname=DBNAME, user=DBUSER, password=DBPASS)
 cursor = conn.cursor()
 
 def loop():
   while True:
     payload = { 'id': CITY_ID, 'appid': OPENWEATHER_APPID, 'units': 'metric' }
-    req = get('http://openweathermap.org/data/2.5/weather', params=payload)
-    print(req.url)
-    print(req.json())
-    # TODO: get temp data
-    apiTemp = 123
+    req = get('http://api.openweathermap.org/data/2.5/weather', params=payload)
+    #print(req.url)
+    output = req.json()
+    apiTemp = output['main']['temp']
 
-    f = open("./t1", "r")
+    f = open(FILE_NAME, "r")
     sensorTemp = getTempFromFile(f)
     f.close()
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
     # TODO: save api temp to db
-    cursor.execute("INSERT INTO readings (sensor) VALUES (%s)", [sensorTemp])
+    cursor.execute("INSERT INTO readings (sensor,api) VALUES (%s,%s)", [sensorTemp, apiTemp])
     conn.commit()
 
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(now, " saved sensor temp ", str(sensorTemp), " and api temp ", str(apiTemp))
 
     sleep(SLEEP_TIME)
