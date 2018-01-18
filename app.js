d1d42b10a6baa9609f85113c0e50f7b20238fd20
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-01-18 23:02:13 +0100

    Serve data from db through the /data endpoint

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const path = require('path')
-
 const { Client } = require('pg')
 
 const app = express()
-const client = new Client()
-
-client.connect()
-
 app.get('/data', (req, res) => {
-  client.query('SELECT $1::text as message', ['Hello world!'])
-    .then((err, res) => {
-      console.log(err ? err.stack : res.rows[0].message) // Hello World!
-      client.end()
-    })
+  const client = new Client()
+  client.connect()
+
+  client.query(`
+    SELECT date as date, sensor as sensor, api as api
+    FROM readings
+    WHERE date >= NOW() - '1 day'::INTERVAL
+  `, (err, queryRes) => {
+
+    if (err) {
+      console.log(err)
+      res.status(500);
+      res.json({ error: err })
+    }
+    else {
+      const response = queryRes.rows
+      res.json(response)
+    }
+
+    client.end()
+  })
 })
 
 app.get('/', (req, res) => {
