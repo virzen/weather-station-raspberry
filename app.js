4ce531355ac5509b8b8730f6e5d8f3ffd4b02f3a
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-01-19 03:18:14 +0100

    Use only ES5 in app.js due to old node on rpi

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require('express')
 const path = require('path')
-const { Client } = require('pg')
+const Client = require('pg').Client
 
 const app = express()
-app.get('/data', (req, res) => {
+app.get('/data', function (req, res) {
   console.log(new Date(), ': /data request')
 
   const client = new Client()
   client.connect()
 
-  client.query(`
-    SELECT date as date, sensor as sensor, api as api
-    FROM readings
-    WHERE date >= NOW() - '1 day'::INTERVAL
-  `, (err, queryRes) => {
+  client.query(
+    "SELECT date as date, sensor as sensor, api as api FROM readings WHERE date >= NOW() - '1 day'::INTERVAL",
+    function (err, queryRes) {
+      if (err) {
+        console.log(err)
+        res.status(500);
+        res.json({ error: err })
+      }
+      else {
+        const response = queryRes.rows
+        res.json(response)
+      }
 
-    if (err) {
-      console.log(err)
-      res.status(500);
-      res.json({ error: err })
+      client.end()
     }
-    else {
-      const response = queryRes.rows
-      res.json(response)
-    }
-
-    client.end()
-  })
+  );
 })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3000, function () { console.log('Listening on port 3000!') })
